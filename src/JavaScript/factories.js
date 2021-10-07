13f5fa8cf86ab4d1a1fe646da957a17254a178fa
Joshua Beychok <joshbeychok3@gmail.com>
2021-10-07 07:34:44 -0400

    created project factory and nested todos inside of it ;

--- src/JavaScript/factories.js
+++ src/JavaScript/factories.js
@@ -38,13 +38,84 @@ const Todo = (title, description, dueDate, priority, notes, project) => {
     props._checklist = arr;
   };
 
-
   return {changeProp, getProps, toggleDone, newCheckBox, moveCheckBox};
 };
 
 const Project = (title, description, color) => {
   //nest the ToDo object inside of here
+  const props = {
+    _title: title,
+    _description: description,
+    _color: color,
+    _todos: [],
+  }
+
+  const changeProp = (prop, val) => {
+    props[`_${prop}`] = val;
+  };
+
+  const getProps = () => {
+    return props;
+  };
+
+  const editTodo = (index, prop, val) => {
+    props._todos[index][`_${prop}`] = val;
+  };
+
+  const toggleDone = (index) => {
+    props._todos[index].toggleDone();
+
+  };
+
+  const createTodo = (title, description, dueDate, priority, notes) => {
+    const newTodo = Todo(title, description, dueDate, priority, notes);
+    props._todos.push(newTodo);
+  };
+
+  const createCheckBox = (index, text) => {
+    props._todos[index].newCheckBox(text);
+  };
+
+  const moveCheckBox = (index, checkIndex, direction) => {
+    props._todos[index].moveCheckBox(checkIndex, direction);
+  };
+
+  const Todo = (title, description, dueDate, priority, notes) => ({
+    
+    _title: title,
+    _description: description,
+    _dueDate: dueDate,
+    _priority: priority,
+    _notes: notes,
+    _checklist: [],
+    _done: false,
+    
+    toggleDone()  {
+      console.log(this._done)
+      this._done = !this._done;
+      console.log(this._done);
+    },
+      
+    newCheckBox(text) {
+      this._checklist.push(text);
+    },
+    
+    moveCheckBox(index, direction) {
+      const arr = this._checklist;
+      const element = arr[index];
+      arr.splice(index, 1);
+      if (direction == 'up') {
+        arr.splice(index-1, 0, element);
+      } else if (direction == 'down') {
+        arr.splice(index+1, 0, element);
+      }
+      this._checklist = arr;
+    }
+    //return {changeProp, getProps, toggleDone, newCheckBox, moveCheckBox};
+  });
+
+  return { changeProp, getProps, createTodo, editTodo, toggleDone, createCheckBox, moveCheckBox }
 }
 
 
-export { Todo }
+export { Project }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import { Todo } from './JavaScript/factories.js'
+import { Project } from './JavaScript/factories.js'
+
+const project = Project("TestProject", "This is a test", "#0000ff");
+console.log(project.getProps());
+project.createTodo("test1", "test2", "test3", "test4", "test5", "test6", "test7");
+console.log(project.getProps());
+project.editTodo(0, 'title', 'Project test Title');
+console.log(project.getProps());
+project.toggleDone(0);
+console.log(project.getProps());
+project.createCheckBox(0, "baltimore wedding");
+project.createCheckBox(0, "do the thing");
+console.log(project.getProps());
+project.moveCheckBox(0,0, "down");
+console.log(project.getProps());
+
+// const todoTwo = Todo("test1", "test2", "test3", "test4", "test5", "test6", "test7");
+// console.log(todoTwo);
+// todoTwo.changeProp('title', 'newTitle');
+// todoTwo.changeProp('dueDate', '9/21/2021');
+// todoTwo.toggleDone();
+// todoTwo.newCheckBox('this is check item');
+// todoTwo.newCheckBox('another check item');
+// todoTwo.moveCheckBox(1, 'up');
+// todoTwo.moveCheckBox(0, 'down');
+// console.log(todoTwo.getProps());
 
 
-
-
-const todoTwo = Todo("test1", "test2", "test3", "test4", "test5", "test6", "test7");
-console.log(todoTwo);
-todoTwo.changeProp('title', 'newTitle');
-todoTwo.changeProp('dueDate', '9/21/2021');
-todoTwo.toggleDone();
-todoTwo.newCheckBox('this is check item');
-todoTwo.newCheckBox('another check item');
-todoTwo.moveCheckBox(1, 'up');
-todoTwo.moveCheckBox(0, 'down');
-console.log(todoTwo.getProps());
